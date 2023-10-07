8fecb48568f64fccf5980a644a1848036344d013
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-07 14:39:14 +0700

    responsive home page

---
+++ linkedin/src/app/home/components/tabs/tabs.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+import { ModalComponent } from '../start-post/modal/modal.component';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+})
+export class TabsComponent implements OnInit {
+  constructor(public modalController: ModalController) {}
+
+  ngOnInit() {}
+
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: ModalComponent,
+      cssClass: 'my-custom-class2',
+    });
+    await modal.present();
+    const { role, data } = await modal.onDidDismiss();
+    console.log('role', role, '- data ', data);
+  }
+}
